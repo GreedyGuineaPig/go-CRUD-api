3a7afb9844da81e0e0338b52a4f6f472b3251e22
srk400krz <srk400krz@gmail.com>
2022-07-23 18:33:47 +0000

    added root page

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
@@ -10,4 +18,17 @@ type Movie struct {
 type Director struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "this is a root directory")
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", rootHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
